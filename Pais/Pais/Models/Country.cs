243801ecf06b3a9b015050ca801096012be1571a
Diego Peredo <a2019118858@estudiantes.upsa.edu.bo>
2021-05-08 10:55:59 -0400

    se agregó datacontext

---
+++ Pais/Pais/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pais.Models
+{
+    public class Country
+    {
+        [Key]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public double Population { get; set; }
+        public string Timezones { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
